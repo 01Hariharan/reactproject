5b4587d09fbf00dba270d9494ca70c50da1e04aa
01Hariharan <hariirah657@gmail.com>
2025-05-30 20:45:55 +0530

    Addtocartfunction

--- src/Shoppingsite.js
+++ src/Shoppingsite.js
@@ -1,36 +1,40 @@
 import React, { useState } from 'react';
 import "./Shoppingsite.css";
 import ProductCard from './components/ProductCard';
-import img1 from "./assets/sony.jpg"
-import img2 from "./assets/iphone.jpg"
-import img3 from "./assets/mi.jpg"
-import img4 from "./assets/samsung.jpg"
+import img1 from "./assests/sony.jpg"
+import img2 from "./assests/iphone.jpg"
+import img3 from "./assests/mi.jpg"
+import img4 from "./assests/samsung.jpg"
 
 function Shoppingsite() {
   const datas = [
-    {id: 1,
+    {
+      id: 1,
       product_name: "SONY",
       price: 22000,
-      desc: "To download drivers, firmware updates, BIOS, and software"
-      img1: img1,
+      desc: "To download drivers, firmware updates, BIOS, and software",
+      img: img1,
     },
-    {id: 2,
+    {
+      id: 2,
       product_name: "APPLE",
-      price: 10000,
-      desc: "To download drivers, firmware updates, BIOS, and software"
-      img1: img2,
+      price: 100000,
+      desc: "To download drivers, firmware updates, BIOS, and software",
+      img: img2,
     },
-    {id: 3,
+    {
+      id: 3,
       product_name: "MI",
       price: 45000,
-      desc: "To download drivers, firmware updates, BIOS, and software"
-      img1: img3,
+      desc: "To download drivers, firmware updates, BIOS, and software",
+      img: img3,
     },
-    {id: 4,
+    {
+      id: 4,
       product_name: "SAMSUNG",
       price: 50000,
-      desc: "To download drivers, firmware updates, BIOS, and software"
-      img1: img4,
+      desc: "To download drivers, firmware updates, BIOS, and software",
+      img: img4,
     },
   ];
 
@@ -41,23 +45,39 @@ function Shoppingsite() {
   }
 
   console.log(cartItems);
-  
+
   return (
     <div className='container-fluid mainpage'>
       <div className='row'>
         <div className='left_section col-10'>
-          <div classname="row">
+          <div className="row d-flex">
             {datas.map((item) => {
-              return(
-              <ProductCard item={item} handleAddToCart={handleAddToCart} />
+              return (
+                <ProductCard item={item} handleAddToCart={handleAddToCart} />
               );
-              })}
+            })}
           </div>
         </div>
+        <div className='right_section col-2'>
+        <ol class="list-group list-group-numbered">
+          {
+            cartItems.map((citem) => {
+              return (
+                <li class="list-group-item d-flex justify-content-between align-items-start">
+                  <div class="ms-2 me-auto">
+                    <div class="fw-bold">{citem.product_name}</div>
+                    {citem.price}
+                  </div>
+                  <span class="badge text-bg-primary rounded-pill">X</span>
+                </li>
+              );
+            })
+          }
+        </ol>
       </div>
-      
+      </div>
     </div>
-  )
-}
+  
+)}
 
-export default Shoppingsite
+export default Shoppingsite;
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 function ProductCard(props) {
   return (
-    <div class="card" style={{width: "18rem" }}>
-         <img src={props.item.img} class="card-img-top" alt="..."> </img>
-     <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-        <button class="btn btn-primary" onClick={() => {props.handleAddToCart(props.item);}}>Go somewhere</button> 
-     </div>
+    <div class="card" style={{ width: "18rem" }}>
+      <img src={props.item.img} class="card-img-top" alt="..." height={250} />
+      <div class="card-body">
+        <h5 class="card-title">{props.item.product_name}</h5>
+        <p class="card-text">{props.item.desc}</p>
+        <button class="btn btn-primary" onClick={() => { props.handleAddToCart(props.item); }}>Add to cart</button>
+      </div>
     </div>
   );
 }
 
-export default ProductCard
+export default ProductCard;
